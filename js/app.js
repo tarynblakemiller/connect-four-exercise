e4a39935ff7fb6b0bbbbf117278fe535f563786d
Taryn Miller <yourfriendtaryn@gmail.com>
2024-01-23 21:38:20 -0500

    reset game employed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
-let element;
+let columns = 7;
+let rows = 6;
 
-element = document;
+//get the container element
+const boardElement = document.getElementById("board");
 
-console.log(element);
+//storing the board state
+const gameBoard = [];
+
+//generates the board
+function makeBoard() {
+  for (let i = 0; i < rows; i++) {
+    gameBoard[i] = [];
+    for (let j = 0; j < columns; j++) {
+      const circle = document.createElement("div");
+      circle.className = "tile";
+      circle.id = "tile_id";
+      //add it to board
+      boardElement.appendChild(circle);
+
+      gameBoard[i][j] = "0";
+    }
+  }
+}
+console.log(gameBoard);
+makeBoard();
+
+function restart() {
+  const resetTiles = document.getElementsByClassName("tile");
+  for (let i = 0; i < resetTiles.length; i++) {
+    resetTiles[i].style.backgroundColor = "white";
+  }
+}
